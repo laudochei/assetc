9d39d87d8a479c250885bd915f0b7de3ab022cad
Laud Charles Ochei <Laud.Ochei@addenergy.no>
2017-12-11 17:46:06 +0000

    make it better

--- src/main/java/assetc/config/DataConfig.java
+++ src/main/java/assetc/config/DataConfig.java
@@ -22,20 +22,38 @@ public class DataConfig {
        */
 
 
-@Bean
+	@Bean
         public DataSource getDataSource1() {
             DriverManagerDataSource db = new DriverManagerDataSource();
             db.setDriverClassName("com.mysql.jdbc.Driver");
-            db.setUrl("jdbc:mysql://localhost:3306/assetc");
+            db.setUrl("jdbc:mysql://us-cdbr-iron-east-05.cleardb.net:3306/heroku_7ac45a05bab0085?reconnect=true");
             db.setUsername("b4778f9d520c68");
             db.setPassword("132d6c92");
             return db;
         }
+	
 
 
 
 
+	/*
+	@Bean
+    	public DataSource dataSource() throws URISyntaxException {
+        	URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
 
+        	String username = dbUri.getUserInfo().split(":")[0];
+        	String password = dbUri.getUserInfo().split(":")[1];
+        	String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+        	BasicDataSource basicDataSource = new BasicDataSource();
+        	basicDataSource.setUrl(dbUrl);
+        	basicDataSource.setUsername(username);
+        	basicDataSource.setPassword(password);
+
+        	return basicDataSource;
+    	}
+
+	*/
 
 
 
