4fddd960b4b951ba8935058d08b6e730e4464de3
laudochei <33354540+laudochei@users.noreply.github.com>
2018-02-13 17:30:22 +0000

    Add files via upload

---
+++ src/main/java/assetc/validator/LocationException.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assetc.validator;
+
+/**
+ *
+ * @author Laud.Ochei
+ */
+public class LocationException  extends Exception{
+        private static final long serialVersionUID = 1L;
+	private String errorMessage;
+ 
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	public LocationException(String errorMessage) {
+		super(errorMessage);
+		this.errorMessage = errorMessage;
+	}
+	public LocationException() {
+		super();
+	}
+    
+    
+}
